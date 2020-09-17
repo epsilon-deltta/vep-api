459945c4ef19f3466d093a23e0968564322d7599
epsilon-deltta <lovingisluck@naver.com>
2020-09-17 20:25:57 +0900

    solve cors problem

--- djg_api/settings.py
+++ djg_api/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'dna_api.apps.DnaApiConfig',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -49,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
+    'corsheaders.middleware.CorsMiddleware' ,
 ]
 
 ROOT_URLCONF = 'djg_api.urls'
@@ -121,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ALLOW_CREDENTIALS = True
--- dna_api/views.py
+++ dna_api/views.py
@@ -1,3 +1,4 @@
+from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import get_object_or_404,render
 from django.http import HttpResponse ,HttpRequest
 import simplejson as json
@@ -9,7 +10,7 @@ def shaodi(request):
     # latest_question_list = Question.objects.order_by('-pub_date')[:5]
     # context = {'latest_question_list': latest_question_list}
     return render(request, 'dna_api/index.html', context)
-
+@csrf_exempt 
 def index(request):
     # latest_question_list = Question.objects.order_by('-pub_date')[:5]
     # context = {'latest_question_list': latest_question_list}
@@ -18,5 +19,11 @@ def index(request):
     context['dna'] = dna 
     context.update(  maker.get_percentage(dna) )
     
+    response        = HttpResponse(json.dumps(context), content_type="application/json")
+    # response = HttpResponse('success')
+    # response["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
+    # response["Access-Control-Allow-Origin"] = "*"
+    # response["Acess-Control-Max-Age"] = "1000"
+    # response["Access-Control-Allow-Headers"] = "X-Requested-With, Content-Type"
 
-    return HttpResponse(json.dumps(context), content_type="application/json")
+    return  response
